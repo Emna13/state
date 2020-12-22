a8a667c29f3cdd9be683fe0b3236570b243b4c5c
Emna13 <emezni1@gmail.com>
2020-12-22 20:30:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import image from "./image.jpg";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: [
+        {
+          FullName: "Emna Mezni",
+          profession: " Hello ! I'm studying to become a Full Stack Developer",
+        },
+      ],
+      isShow: false,
+      counter: 0,
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  showProfile = () => {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+  };
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    }, 1000);
+  }
+  displayTime=(timeSec)=>{
+    let h=Math.floor(timeSec/3600)
+    let m=Math.floor((timeSec-(h*3600))/60)
+    let s=timeSec-((h*3600)+m*60)
+    return <div>
+      <span>{String(h).padStart(2,0)}:{String(m).padStart(2,0)}:{String(s).padStart(2,0)}</span>
     </div>
-  );
+  }
+
+  render() {
+
+    return (
+      <div className="profile">
+        <div>
+          <h6>{this.displayTime(this.state.counter)}</h6>
+        </div>
+        <Button variant="outline-primary" onClick={this.showProfile}>
+          {this.state.isShow?'Hide Profile':'Show Profile'}
+        </Button>
+        {this.state.isShow ? (
+          <div>
+            {this.state.profile.map((el, index) => (
+              <Card className="card" style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={image} style={{ width: "100%" }} />
+                <Card.Body>
+                  <Card.Title className="title">{el.FullName}</Card.Title>
+                  <Card.Text className="text">
+                    {el.bio}
+                    {el.profession}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        ) : (
+        ""
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
